abf78c2e858c23bee1499b045d56aa13bbfcc5fd
1213清心 <30729159+szh1213@users.noreply.github.com>
2021-02-23 21:31:48 +0800

    Update yiqing.py

--- yiqing.py
+++ yiqing.py
@@ -25,8 +25,7 @@ def report(usr,pas):
     sess.headers['Accept-Language'] = 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6'
     sess.headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;\
         q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
-    r=sess.get('http://yiqing.ctgu.edu.cn/wx/index/login.do?currSchool=ctgu&\
-               CURRENT_YEAR=2019&showWjdc=false&studentShowWjdc=false')
+    r=sess.get('http://yiqing.ctgu.edu.cn/wx/index/login.do?currSchool=ctgu&CURRENT_YEAR=2019&showWjdc=false&studentShowWjdc=false')
     
     data=encode_multipart_formdata({'username':usr,'password':pas},
                                    '----WebKitFormBoundary5lPtCfVeRqiu7n6h')
